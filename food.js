3374f57fe93fc26a97a3f7449a244473cb8de3f8
lavell119 <leewhiz119@gmail.com>
2021-10-10 21:50:55 -0400

    created snake expansion and random food position

--- food.js
+++ food.js
@@ -1,7 +1,16 @@
-let food = { x: 0, y: 0 }
+import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid.js'
+
+let food = getRandomFoodPosition()
+const EXPANSION_RATE = 1
+
+
 
 export function update() {
-
+    if (onSnake(food)) {
+        expandSnake(EXPANSION_RATE)
+        food = getRandomFoodPosition()
+    }
 }
 
 
@@ -13,4 +22,12 @@ export function draw(gameBoard) {
         foodElement.classList.add('food')
         gameBoard.appendChild(foodElement)    
     
+}
+
+function getRandomFoodPosition() {
+    let newFoodPosition
+    while (newFoodPosition == null || onSnake(newFoodPosition)) {
+        newFoodPosition = randomGridPosition()
+    }
+    return newFoodPosition
 }
